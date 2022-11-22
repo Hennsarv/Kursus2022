eaf8f150ab3d264c92aa0f5a1d596c23d14419fb
Henn Sarv <sarviktaat@hotmail.com>
2022-11-22 09:43:48 +0200

    muuudatus

--- Konsole2022/Program.cs
+++ Konsole2022/Program.cs
@@ -2,10 +2,10 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args) 
         {
             Console.WriteLine("Hello, World!");
-
+            Console.WriteLine("Koolitusel tehtud täiendus");
         }
     }
 }
