eb4e33985818458591fd4714556a8376177bc48f
Hennsarv <henn@sarv.ee>
2022-11-22 09:36:36 +0200

    Esimene versioon

---
+++ Konsole2022/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Konsole2022
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+        }
+    }
+}
