34a1717a4a25dc036821093e301c44d8f0b95aec
Henn Sarv <sarviktaat@hotmail.com>
2022-11-28 09:04:37 +0200

    NV ei teinud muudatusi

--- Klassid/Inimene.cs
+++ Klassid/Inimene.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,30 +9,72 @@ namespace Klassid
 {
     public class Inimene // reference
     {
+        static int nr = 0;
+        readonly int _Nr = ++nr;
+        public int Nr => _Nr;
+
+        public static int Inimesi => nr;
+
+        public static string Inimest =>
+            nr == 0 ? "meil pole ühtegi inimest" :
+            nr == 1 ? "meil on üks inimene" :
+            $"meil on {nr} inimest";
+
+        private string _IK;
+        public string IK { get => _IK; set => _IK = _IK == "" ? value : _IK; }
+        
+        public static Dictionary<int, Inimene> Inimesed 
+            = new Dictionary<int, Inimene>();
+
+        public Inimene() => Inimesed.Add(nr, this);
+
+
+
         // klassi liikmed ehk Member
-        public string Nimi;
+        private string _Eesnimi;
+        private string _Perenimi;
+        public string Eesnimi
+        {
+            get => _Eesnimi;
+            set => _Eesnimi = value.ToProper();
+        }
+        public string Perenimi
+        {
+            get => _Perenimi;
+            set => _Perenimi = value.ToProper();
+        }
+
+        public string Nimi
+        {
+            get => _Eesnimi + " " + Perenimi;
+            set
+            {
+                var nimed = value.Split(' ');
+                Eesnimi = nimed[0];
+                Perenimi = nimed[nimed.Length - 1]; 
+            }
+        }
         public DateTime SünniAeg { get; set; } = DateTime.Today;
         private int _Palk;
         public int Palk
         {
-            get => _Palk; 
-            set => _Palk = value > _Palk ? value : _Palk; 
+            get => _Palk;
+            set => _Palk = value > _Palk ? value : _Palk;
         }
         //private int _kinganumber;
-        public int Kinganumber 
-        { get  ; set; }
+        public int Kinganumber
+        { get; set; }
 
 
-        public int getPalk() => _Palk;
-        public void setPalk(int uusPalk) => _Palk = uusPalk > _Palk ? uusPalk : _Palk;
-        
+  //      public int getPalk() => _Palk;
+  //      public void setPalk(int uusPalk) => _Palk = uusPalk > _Palk ? uusPalk : _Palk;
+
         // read-only property - puudub set
         public int Vanus => (DateTime.Today - SünniAeg).Days * 4 / 1461;
-            
-        
 
-        public override string ToString() => $"{Nimi} saab palka {Palk} ja on sündinud {SünniAeg:dd.MM.yyyy}";
 
+
+        public override string ToString() => $"{Nr}. {Nimi} saab palka {Palk} ja on sündinud {SünniAeg:dd.MM.yyyy}";
 
 
 
--- Klassid/Program.cs
+++ Klassid/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.CodeDom;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,6 +10,11 @@ namespace Klassid // neist räägime veel
 {
     static class Program
     {
+        internal static string ToProper(this string x) =>
+            x.Trim().Substring(0, 1).ToUpper()
+            +
+            x.Trim().Substring(1).ToLower()
+            ;
 
         // näidiseks veel üks funktsioon
         #region MyRegion
@@ -28,27 +34,68 @@ namespace Klassid // neist räägime veel
 
         static void Main()
         {
+            
             Inimene henn = new Inimene
             {
                 Nimi = "Henn Sarv",
                 SünniAeg = new DateTime(1955,3,7),
                 Palk = 4000
             };
-
+            
             henn.Palk += 1000;
+            henn.Perenimi = "sarviktaat";
 
+            Console.WriteLine(henn.ToString());
+            Console.WriteLine(new Inimene { Nimi = "jaagup"});
+
+            new Inimene { Nimi = "Luarvik" };
+
+            //Console.WriteLine(henn.Vanus);
+
+            Console.WriteLine(Inimene.Inimest);
+
+            Console.WriteLine(Inimene.Inimesed[3]);
+
+            List<string> nimed = new List<string>();
+            
+            Dictionary<int, NimiSugu> nimedsood = new Dictionary<int, NimiSugu>();
+            nimedsood.Add(1, new NimiSugu { Nimi = "Henn", Sugu = Sugu.Mees });
+            nimedsood.Add(2, new NimiSugu { Nimi = "Anne", Sugu = Sugu.Naine });
+            nimedsood.Add(3, new NimiSugu { Nimi = "Malle", Sugu = Sugu.Naine });
+            nimedsood.Add(4, ("Kalle", Sugu.Mees));
+
+            Dictionary<int, (string nimi, Sugu sugu)> ns = new Dictionary<int, (string, Sugu)>();
+            ns.Add(1, ("Henn", Sugu.Mees));
+
+            foreach (var x in nimedsood) Console.WriteLine(x);
+
+            Console.WriteLine(ns[1].sugu);
             
 
-            Console.WriteLine(henn);
-            Console.WriteLine(new Inimene { Nimi = "Jaagup"});
-
-            Console.WriteLine(henn.Vanus);
 
         }
 
 
-        
 
+
+    }
+
+    enum Sugu { Mees, Naine}
+
+    enum Mast { Risti, Ruutu, Ärtu, Poti, Pada=3}
+
+    class NimiSugu
+    {
+        public string Nimi;
+        public Sugu Sugu;
+
+        public NimiSugu() { }
+        public NimiSugu(string nimi, Sugu sugu) => (Nimi, Sugu) = (nimi, sugu);
+
+        public override string ToString() => $"{Sugu} {Nimi}";
+
+        public static implicit operator NimiSugu((string nimi, Sugu sugu) x) => new NimiSugu(x.nimi, x.sugu);
+        
     }
     
     
