18a215eb80faad5906a72bd1995462b92bae7497
Henn Sarv <sarviktaat@hotmail.com>
2022-11-28 11:29:14 +0200

    saime andmete lugemisega failist hakkama

--- ControllerApp/Program.cs
+++ ControllerApp/Program.cs
@@ -7,6 +7,17 @@ using System.IO;
 
 namespace ControllerApp
 {
+    class Inimene
+    {
+        public string Eesnimi { get; set; }
+        public string Perenimi { get; set; }
+        public int Vanus { get; set; }
+
+        public string Nimi { get { return $"{Eesnimi} {Perenimi}"; } }
+
+        public override string ToString() => $"Inimene: {Nimi} vanusega: {Vanus}";
+    }
+
     static class Program
     {
         static string FolderName = @"..\..\";
@@ -21,6 +32,7 @@ namespace ControllerApp
         2 - teine valik
         n - loe näidisfail
         t - salvesta tulemus
+        a - loe andmed
         väljumiseks vakita enter
         ";
 
@@ -46,7 +58,7 @@ namespace ControllerApp
                         break;
                     case "2":
                         Console.WriteLine("teine valik oli see");
-                        break; 
+                        break;
                     #endregion
                     case "n":
                         sisu = File.ReadAllLines(FileName);
@@ -63,6 +75,24 @@ namespace ControllerApp
                             File.WriteAllLines(FileName2, sisu);
                         }
                         break;
+                    case "a":
+                        // faili peame mällu ikka lugema (kui ta seal juba ei ole)
+                        if (sisu == null) sisu = File.ReadAllLines(FileName);
+                        List<Inimene> inimesed = new List<Inimene>();
+                        foreach(var x in sisu) 
+                        {
+                            var o = x.Split(',');
+                            Inimene i = new Inimene();
+                            i.Eesnimi = o[0].Trim();
+                            i.Perenimi= o[1].Trim();
+                            i.Vanus = int.Parse(o[2].Trim());
+                            inimesed.Add(i);
+                        }
+                        foreach (var x in inimesed) Console.WriteLine(x);
+
+                        break;
+
+
 
                 }
             }
