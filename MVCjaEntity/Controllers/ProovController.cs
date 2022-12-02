574b5491060e55399f53412676c7acfa5dd442e6
Henn Sarv <sarviktaat@hotmail.com>
2022-12-02 11:08:55 +0200

    tegime REST kontrollereid pisut

---
+++ MVCjaEntity/Controllers/ProovController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MVCjaEntity.Controllers
+{
+    public class ProovController : ApiController
+    {
+        static List<string> ProovNames = new List<string>
+        {
+            "Henn", "Ants", "Peeter"
+        };
+        // GET: api/Proov
+        public IEnumerable<string> Get()
+        {
+            return ProovNames.ToArray();
+        }
+
+        // GET: api/Proov/5
+        public string Get(int id)
+        {
+            return ProovNames[id];
+        }
+
+        // POST: api/Proov
+        public void Post([FromBody]string value)
+        {
+            ProovNames.Add(value);
+        }
+
+        // PUT: api/Proov/5
+        public void Put(int id, [FromBody]string value)
+        {
+            ProovNames[id] = value;
+        }
+
+        // DELETE: api/Proov/5
+        public void Delete(int id)
+        {
+            ProovNames.RemoveAt(id);
+        }
+    }
+}
---
+++ MVCjaEntity/Controllers/TestController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MVCjaEntity.Controllers
+{
+    public class TestController : ApiController
+    {
+        static List<string> nimed = new List<string>
+        {
+            "Henn", "Ants", "Peeter"
+        };
+
+
+        // GET: api/Test
+        public IEnumerable<string> Get()
+        {
+            return nimed;
+        }
+
+        // GET: api/Test/5
+        public string Get(int id)
+        {
+            return nimed[id];
+        }
+
+        // POST: api/Test
+        public void Post([FromBody]string value)
+        {
+            nimed.Add(value);
+        }
+
+        // PUT: api/Test/5
+        public void Put(int id, [FromBody]string value)
+        {
+            nimed[id] = value;
+        }
+
+        // DELETE: api/Test/5
+        public void Delete(int id)
+        {
+            nimed.RemoveAt(id);
+        }
+    }
+}
